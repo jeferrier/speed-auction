77ffa5edbae4c418e2242ad5824df15d9a81ad0d
James Ferrier <jeferrier@germty.com>
2015-11-09 19:18:35 -0500

    Adding paperclip and S3

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   has_one :auction
+  validates_attachment_content_type :headline_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_presence :headline_image
 end
