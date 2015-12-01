f49986aa56c9360dfe7dcc4fa2aa84bdb1f51319
unknown <jeharrison@IT05780.valdosta.edu>
2015-11-30 19:59:00 -0500

    working on billing / payment ti-in

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :show_sign_in
   before_action :user_logged_in
+  before_action :user_owns_item
+  before_action :item_is_past
   # GET /items
   # GET /items.json
   def index
@@ -75,7 +77,9 @@ class ItemsController < ApplicationController
   def update
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        @item.auction.current_bid = @item.starting_price
+        @item.auction.save
+        format.html { redirect_to view_my_auctions_path, notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit }
@@ -103,6 +107,18 @@ class ItemsController < ApplicationController
 
     def user_logged_in
       if @show_sign_in
+        redirect_to root_path
+      end
+    end
+    
+    def user_owns_item
+      if @user.id != @item.user_id
+        redirect_to root_path
+      end
+    end
+    
+    def item_is_past
+      if @item.auction.begin_date <= DateTime.now
         redirect_to root_path
       end
     end
