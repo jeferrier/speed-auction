8b1659e3361632ce7e3a200faaa6af74feaf80ba
unknown <jeharrison@IT05850.valdosta.edu>
2015-12-02 19:54:16 -0500

    validations

--- app/models/payment_detail.rb
+++ app/models/payment_detail.rb
@@ -1,8 +1,10 @@
 class PaymentDetail < ActiveRecord::Base
   belongs_to :user
   has_one :billing_info
-  validates :credit_card_number, length: { is: 16 }, presence: true, if: "credit_card.true?"
-  validates :credit_card_expiration_date, length: { is: 5 }, presence: true, if: "credit_card.true?"
-  validates :credit_card_security_code, length: { in: 3..4 }, presence: true, if: "credit_card.true?"
-  validates :paypal_email_address, presence: true, if: "paypal.true?"
+  validates :credit_card, presence: true, if: "paypal == false"
+  validates :paypal, presence: true, if: "credit_card == false"
+  validates :credit_card_number, length: { is: 16 }, presence: true, if: "credit_card == true"
+  validates :credit_card_expiration_date, length: { is: 5 }, presence: true, if: "credit_card == true"
+  validates :credit_card_security_code, length: { in: 3..4 }, presence: true, if: "credit_card == true"
+  validates :paypal_email_address, presence: true, if: "paypal == true"
 end
