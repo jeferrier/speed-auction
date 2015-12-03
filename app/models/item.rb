d95dd4dcc4beb1ca606088165dbc1018e47835aa
unknown <jeharrison@IT05850.valdosta.edu>
2015-12-02 19:44:35 -0500

    validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,11 @@ class Item < ActiveRecord::Base
   has_attached_file :headline_image
   validates_attachment_content_type :headline_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates_attachment_presence :headline_image
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :starting_price, presence: true, numericality: {greater_than: 0}, length: { maximum: 32 }
+  validates :street_address, presence: true, length: { maximum: 32 }
+  validates :city, presence: true, length: { maximum: 15 }
+  validates :state, presence: true, length: { is: 2 }
+  validates :zip, presence: true, length: { is: 5 }, numericality: { only_integer: true }
 end
--- app/models/payment_detail.rb
+++ app/models/payment_detail.rb
@@ -1,4 +1,8 @@
 class PaymentDetail < ActiveRecord::Base
   belongs_to :user
   has_one :billing_info
+  validates :credit_card_number, length: { is: 16 }, presence: true, if: "credit_card.true?"
+  validates :credit_card_expiration_date, length: { is: 5 }, presence: true, if: "credit_card.true?"
+  validates :credit_card_security_code, length: { in: 3..4 }, presence: true, if: "credit_card.true?"
+  validates :paypal_email_address, presence: true, if: "paypal.true?"
 end
