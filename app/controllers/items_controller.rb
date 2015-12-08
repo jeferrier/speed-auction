5b8aae4e3a4bdb81185ca36612c5131eff6fb0ee
James Ferrier <jeferrier@germty.com>
2015-12-08 15:35:39 -0500

    Touch ups for heroku

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
           largest_date = a.end_date
         end
       end
-       @auction.begin_date = largest_date - 1.day #could fix the day bug?
+       @auction.begin_date = largest_date
       @auction.end_date = @auction.begin_date + 1.day
     end
 
